a58f3d2c3d0958e8f8f586bf92c4b8e924b119e3
Robert Van Ysendyck <rvy@go-models.eu>
2024-03-26 02:11:06 +0100

    feat: switch from HTML parsing to JSON parsing
    BREAKING CHANGE:

--- README.md
+++ README.md
@@ -1,71 +1,78 @@
 # TrustpilotReviewScraper
 
-`TrustpilotReviewScraper` is a Python script designed to scrape reviews from Trustpilot.com. It extracts detailed review information including the reviewer's name, review count, country, rating, review date, review title, review content, and date of experience. The tool supports scraping across multiple pages and can fetch reviews in all available languages, outputting the data into a CSV file for further analysis.
+Welcome to **TrustpilotReviewScraper**, a Python-based tool designed for scraping detailed reviews from Trustpilot.com.
 
-## Requirements
+With TrustpilotReviewScraper, you can easily extract reviews for a specific domain on Trustpilot, filtering by star ratings, date ranges, languages, and more. With an easy-to-use command-line interface, you can quickly collect and analyze Trustpilot reviews for various purposes, such as sentiment analysis, customer feedback, and market research.
 
-- Python 3.12.1 (May work with earlier versions of Python 3, but not tested)
+- 🔍 **Comprehensive Data Collection**: Extract all available data for Trustpilot reviews, including ratings, titles, content, dates, and more.
+- 🌍 **Multi-Language Support**: Fetch reviews in all available languages, allowing for global data analysis.
+- 📄 **Multi-Page Scraping**: Navigates and collects data from multiple pages automatically.
+- 💾 **Flexible Output Options**: Offers CSV, JSON, or both formats for output, catering to various data processing needs.
+- 🚿 **Customizable Filtering**: Filter reviews by star ratings, date ranges, search keywords, languages, and more.
+- 🔀 **Sorting Capabilities**: Sort reviews by various fields, such as rating, date, relevance, etc.
+- 💻 **Easy-to-Use Command-Line Interface**: Simple and intuitive command-line interface for easy data collection.
 
-## Setup
+## Getting Started
 
-To use `TrustpilotReviewScraper`, follow these setup instructions:
+### Prerequisites
+- Tested with Python 3.12.1. Earlier versions may not be compatible.
 
+### Installation
 1. **Clone the Repository**
-
-    ```
-    git clone https://github.com/YOUR_USERNAME/TrustpilotReviewScraper.git
-    cd TrustpilotReviewScraper
-    ```
-
-2. **Create and Activate a Virtual Environment**
-
-    - For Unix/macOS:
-
-        ```
-        python3 -m venv env
-        source env/bin/activate
-        ```
-
-    - For Windows:
-
-        ```
-        python -m venv env
-        .\env\Scripts\activate
-        ```
-
-3. **Install Required Packages**
-
-    ```
-    pip install -r requirements.txt
-    ```
-
+Start by cloning the repository to your local machine:
+```sh
+git clone https://github.com/YOUR_USERNAME/TrustpilotReviewScraper.git
+cd TrustpilotReviewScraper
+```
+2. **Set Up a Virtual Environment**
+Creating a virtual environment is recommended to avoid any conflicts with other Python projects:
+- Unix/macOS:
+```sh
+python3 -m venv env
+source env/bin/activate
+```
+- Windows:
+```cmd
+python -m venv env
+.\\env\\Scripts\\activate 
+```
+4. **Install Required Packages**
+Install all the necessary packages using pip:
+```sh
+pip install -r requirements.txt
+```
 ## Usage
-
-To start scraping reviews from a specific domain on Trustpilot, run the script as follows:
-
-```python 
+To scrape reviews for a specific domain on Trustpilot, use the following command, replacing `[domain]` with the actual domain you wish to scrape:
+```sh
 python trustpilot_scraper.py [domain]
 ```
-Replace `[domain]` with the Trustpilot domain you wish to scrape. For example:
-python trustpilot_scraper.py example.com
+### Required Argument
+- `domain`: The domain to scrape reviews for, without the need for a flag. It is the first positional argument.
 
+### Optional Arguments
+- `--stars [N ...]`: Filter reviews by star ratings. Accepts multiple values. For instance, `--stars 4 5` will only fetch reviews rated with 4 or 5 stars.
 
-The script will scrape reviews and save them into a CSV file named `reviews_[domain].csv`.
+- `--date`: Filter reviews by date. Options include 'last30days', 'last3months', 'last6months' and 'last12months'. Example usage: `--date last30days`.
+
+- `--search`: Filter reviews by a search keyword. Example usage: `--search "excellent service"`.
+
+- `--languages`: Specify the language of reviews to fetch (e.g., 'en' for English, 'es' for Spanish). The default is 'all', which collects reviews in all languages. Example usage: `--languages en`.
+
+- `--verified`: If used, the script will only fetch reviews that are verified. No additional value needs to be specified. Example usage: `--verified`.
+
+- `--replies`: If used, the script will only fetch reviews that have replies. No additional value needs to be specified. Example usage: `--replies`.
+
+- `--sort-by`: Specify the field to sort reviews by. Options include 'rating', 'date', etc. The default is 'published_date'. Example usage: `--sort-by rating`.
+
+- `--sort-order`: Specify the order to sort reviews in. Options are 'asc' for ascending and 'desc' for descending. The default is 'asc'. Example usage: `--sort-order desc`.
+
+- `--output`: Choose the output format of the scraped data. Options are 'csv', 'json', or 'both'. The default is 'csv'. Example usage: `--output both`.
 
 ## Contributing
-
-Contributions are welcome! Please feel free to submit a pull request or open an issue if you have suggestions or improvements.
+Contributions are welcome! Feel free to fork the repository, make changes, and submit pull requests. If you have any suggestions or issues, please open an issue in the GitHub repository.
 
 ## License
-
 This project is licensed under the MIT License - see the LICENSE file for details.
 
 ## Disclaimer
-
-This software is provided for educational purposes only. By using this software, you agree that it is solely for personal and educational use. The developer(s) of this project do not endorse or promote the use of this software for any purposes that may violate specific terms of service, legal agreements, or laws.
-It is the responsibility of the user to ensure that their use of this software complies with all relevant terms of service and laws. The user should also be aware of and respect the ethical considerations involved in web scraping, such as respecting the robots.txt file of websites, avoiding excessive requests that can impact service performance, and not accessing or collecting data without permission.
-The developer(s) assume no liability for any misuse of this software or any violations of terms or laws resulting from its use. Users are encouraged to consult a legal professional if they have questions about the legal implications of their intended use of this software.
-
-
-
-
+This tool is for educational and research purposes only. Please use responsibly and ethically.
--- trustpilot.py
+++ trustpilot.py
@@ -1,119 +1,251 @@
 import argparse
 import csv
+import json
 import logging
 import random
+import sys
 import time
 from datetime import datetime
+from urllib.parse import parse_qs, urlparse
+from urllib.robotparser import RobotFileParser
 
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
+session = requests.Session()
 
-def generate_url(domain: str, page: int, all_languages: bool) -> str:
+
+def parse_arguments():
     """
-    Generate the URL for the given domain and page number.
+    Parses command-line arguments.
+    :return: Namespace object with parsed command-line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Scrape reviews from Trustpilot for a given domain."
+    )
+    parser.add_argument("domain", help="The domain to scrape reviews for.", type=str)
+    parser.add_argument(
+        "--stars",
+        nargs="*",
+        help="Filter reviews by stars. Accept multiple values (e.g., --stars 4 5).",
+        type=int,
+    )
+    parser.add_argument(
+        "--date",
+        help="Filter reviews by date (e.g., last30days, last3months, last6months).",
+        type=str,
+    )
+    parser.add_argument("--search", help="Filter reviews by search keyword.", type=str)
+    parser.add_argument(
+        "--languages",
+        help="Filter reviews by language (e.g., es for Spanish, all for all languages).",
+        type=str,
+        default="all",
+    )
+    parser.add_argument(
+        "--verified",
+        action="store_true",
+        help="Filter reviews to show only verified reviews.",
+    )
+    parser.add_argument(
+        "--replies",
+        action="store_true",
+        help="Filter reviews to show only reviews with replies.",
+    )
+    parser.add_argument(
+        "--sort-by",
+        help="Sort reviews by a specified field (e.g., 'rating', 'date').",
+        type=str,
+        default="published_date",
+    )
+    parser.add_argument(
+        "--sort-order",
+        choices=["asc", "desc"],
+        default="asc",
+        help="Sort order: 'asc' for ascending, 'desc' for descending. Default is ascending.",
+    )
+    parser.add_argument(
+        "--output",
+        choices=["csv", "json", "both"],
+        default="csv",
+        help="Specify the output format: 'csv' for CSV file, 'json' for JSON file, or 'both' for both formats. Default is 'csv'.",
+    )
+
+    return parser.parse_args()
+
+
+def generate_url(domain: str, page: int, args) -> str:
+    """
+    Generate the URL for the given domain, page number, and additional filters in a more concise manner.
     :param domain: The domain to get the reviews for.
     :param page: The page number.
-    :param all_languages: Whether to get reviews in all languages.
-    :return: The URL for the given domain and page number.
+    :param args: Parsed command-line arguments containing filters.
+    :return: The URL for the given domain and page number with query parameters.
     """
-    url = f"https://www.trustpilot.com/review/{domain}?page={page}"
-    if all_languages:
-        url += "&languages=all"
-    return url
+    base_url = f"https://www.trustpilot.com/review/{domain}"
+    params = {"page": page} if page != 1 else {}
+
+    if args.stars:
+        for star in args.stars:
+            params[f"stars"] = star
+    if args.date:
+        params["date"] = args.date
+    if args.search:
+        params["search"] = args.search
+    if args.languages:
+        params["languages"] = args.languages
+    if args.verified:
+        params["verified"] = "true"
+    if args.replies:
+        params["replies"] = "true"
+
+    query_string = "&".join([f"{key}={value}" for key, value in params.items()])
+    return f"{base_url}?{query_string}" if params else base_url
 
 
-def get_html(url: str) -> BeautifulSoup:
+def is_allowed_by_robots_txt(url: str, user_agent: str) -> bool:
     """
-    Get the HTML content of the given URL.
+    Check if the given user agent is allowed to scrape the URL according to robots.txt rules.
+    :param url: The base URL of the website to check.
+    :param user_agent: The User-Agent string of the scraper.
+    :return: True if allowed, False otherwise.
+    """
+    parser = RobotFileParser()
+    parser.set_url(f"{url}/robots.txt")
+    parser.read()
+    return parser.can_fetch(user_agent, url)
+
+
+def iso_to_datetime(iso_str: str) -> datetime | None:
+    """
+    Convert an ISO 8601 formatted string to a datetime object.
+    :param iso_str: The ISO 8601 formatted string.
+    :return: The datetime object.
+    """
+    if iso_str:
+        # Remove the 'Z' and convert to datetime
+        return datetime.fromisoformat(iso_str.replace("Z", "+00:00"))
+    return None
+
+
+def get_html(url: str) -> BeautifulSoup | None:
+    """
+    Get the HTML content of the given URL using a persistent session.
     :param url: The URL to get the HTML content from.
     :return: BeautifulSoup object containing the HTML content.
     """
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
-    }
-    response = requests.get(url, headers=headers)
+    ua = UserAgent()
+    session.headers.update({"User-Agent": ua.random})
+    response = session.get(url)
+
+    final_url = response.url
+    initial_params = parse_qs(urlparse(url).query)
+    final_params = parse_qs(urlparse(final_url).query)
+    if len(initial_params) > 1 and initial_params != final_params:
+        logger.error(f"Redirected to {final_url}. Unexpected redirection detected.")
+        sys.exit(1)
+
+    if response.status_code == 404:
+        return None
+
     if response.status_code == 200:
-        return BeautifulSoup(response.content, "lxml")
+        html_content = BeautifulSoup(response.content, "lxml")
+        return html_content
     response.raise_for_status()
 
 
-def get_reviews(html: BeautifulSoup) -> list[dict]:
+def get_reviews_data(html: BeautifulSoup) -> list[dict]:
     """
-    Get the reviews from the BeautifulSoup object containing the HTML content.
+    Get the reviews data from the HTML content.
     :param html: BeautifulSoup object containing the HTML content.
-    :return: List of dictionaries containing the review details.
+    :return: List of dictionaries containing the review data.
     """
-    page_reviews = []
-    for review_div in html.find_all(
-        "div",
-        class_="styles_cardWrapper__LcCPA styles_show__HUXRb styles_reviewCard__9HxJJ",
-    ):
-        review = {}
+    script_tag = html.find("script", {"id": "__NEXT_DATA__"})
+    return json.loads(script_tag.string)["props"]["pageProps"]["reviews"]
 
-        # Get name of the reviewer
-        rating_span = review_div.find("span", {"data-consumer-name-typography": True})
-        if rating_span:
-            review["name"] = rating_span.text
 
-        # Get the review count of the reviewer
-        review_count_div = review_div.find("div", {"data-consumer-reviews-count": True})
-        if review_count_div:
-            review["review_count"] = int(
-                review_count_div["data-consumer-reviews-count"]
-            )
+def parse_review(review: dict) -> dict:
+    """
+    Parse the review data and extract the relevant information.
+    :param review: Dictionary containing the raw review data.
+    :return: Dictionary containing the processed review data.
+    """
+    parsed_review = {
+        "id": review.get("id"),
+        "filtered": review.get("filtered"),
+        "pending": review.get("pending"),
+        "text": review.get("text"),
+        "rating": review.get("rating"),
+        "title": review.get("title"),
+        "likes": review.get("likes"),
+        "report": review.get("report"),
+        "has_unhandled_reports": review.get("hasUnhandledReports"),
+        "language": review.get("language"),
+        "location": review.get("location"),
+        "consumers_review_count_on_same_domain": review.get(
+            "consumersReviewCountOnSameDomain"
+        ),
+        "consumers_review_count_on_same_location": review.get(
+            "consumersReviewCountOnSameLocation"
+        ),
+    }
 
-        # Get the country of the reviewer
-        country_element = review_div.find(
-            "div", {"data-consumer-country-typography": "true"}
+    # Date information
+    dates = review.get("dates", {})
+    parsed_review["published_date"] = iso_to_datetime(dates.get("publishedDate"))
+    parsed_review["experienced_date"] = iso_to_datetime(dates.get("experiencedDate"))
+    parsed_review["updated_date"] = iso_to_datetime(dates.get("updatedDate"))
+
+    # Consumer information
+    consumer = review.get("consumer", {})
+    parsed_review["display_name"] = consumer.get("displayName")
+    parsed_review["image_url"] = consumer.get("imageUrl")
+    parsed_review["review_count"] = consumer.get("numberOfReviews")
+    parsed_review["country_code"] = consumer.get("countryCode")
+    parsed_review["has_image"] = consumer.get("hasImage")
+    parsed_review["consumer_verified"] = consumer.get("isVerified")
+
+    # Verification labels
+    labels = review.get("labels", {}).get("verification", {})
+    parsed_review["review_verified"] = labels.get("isVerified")
+    parsed_review["review_verification_level"] = labels.get("verificationLevel")
+    parsed_review["review_verification_source"] = labels.get("verificationSource")
+    parsed_review["review_verification_date"] = labels.get("createdDateTime")
+    parsed_review["review_source_name"] = labels.get("reviewSourceName")
+    parsed_review["has_dach_exclusion"] = labels.get("hasDachExclusion")
+
+    # Reply information
+    reply = review.get("reply")
+    if reply:
+        parsed_review["reply_message"] = reply.get("message")
+        parsed_review["reply_published_date"] = iso_to_datetime(
+            reply.get("publishedDate")
         )
-        if country_element:
-            # The country code or name might be in the next span element after the SVG
-            country_code_span = country_element.find_next("span")
-            if country_code_span:
-                review["country"] = country_code_span.text.strip()
+        parsed_review["reply_updated_date"] = iso_to_datetime(reply.get("updatedDate"))
+    else:
+        parsed_review["reply_message"] = None
+        parsed_review["reply_published_date"] = None
+        parsed_review["reply_updated_date"] = None
 
-        # Get the rating of the review
-        rating_div = review_div.find("div", {"data-service-review-rating": True})
-        if rating_div:
-            review["rating"] = int(rating_div["data-service-review-rating"])
-
-        # Get the review date
-        review_date_time = review_div.find(
-            "time", {"data-service-review-date-time-ago": True}
-        )
-        if review_date_time:
-            review["review_date"] = datetime.fromisoformat(review_date_time["datetime"])
-
-        # Get the title of the review
-        title_h2 = review_div.find("h2", {"data-service-review-title-typography": True})
-        if title_h2:
-            review["title"] = title_h2.text.strip()
-
-        # Get the content of the review
-        content_p = review_div.find("p", {"data-service-review-text-typography": True})
-        if content_p:
-            review["content"] = content_p.text.strip()
-
-        # Get the date of the experience
-        date_of_experience_p = review_div.find(
-            "p", {"data-service-review-date-of-experience-typography": True}
-        )
-        if date_of_experience_p:
-            date_of_experience_str = date_of_experience_p.text.split(":")[-1].strip()
-            review["date_of_experience"] = datetime.strptime(
-                date_of_experience_str, "%B %d, %Y"
-            )
-
-        page_reviews.append(review)
-
-    return page_reviews
+    return parsed_review
 
 
-def write_reviews_to_csv(reviews: list[dict], filename: str):
+def process_reviews(reviews_data: list[dict]) -> list[dict]:
+    """
+    Process the reviews data and extract the relevant information.
+    :param reviews_data: List of dictionaries containing the raw review data.
+    :return: List of dictionaries containing the processed review data.
+    """
+    return [parse_review(review) for review in reviews_data]
+
+
+def write_reviews_to_csv(reviews: list[dict], filename: str) -> None:
     """
     Write the reviews to a CSV file.
     :param reviews: List of dictionaries containing the review details.
@@ -123,45 +255,140 @@ def write_reviews_to_csv(reviews: list[dict], filename: str):
         writer = csv.DictWriter(
             file,
             fieldnames=[
-                "name",
-                "review_count",
-                "country",
+                "id",
+                "filtered",
+                "pending",
+                "text",
                 "rating",
-                "review_date",
                 "title",
-                "content",
-                "date_of_experience",
+                "likes",
+                "report",
+                "has_unhandled_reports",
+                "language",
+                "location",
+                "consumers_review_count_on_same_domain",
+                "consumers_review_count_on_same_location",
+                "published_date",
+                "experienced_date",
+                "updated_date",
+                "display_name",
+                "image_url",
+                "review_count",
+                "country_code",
+                "has_image",
+                "consumer_verified",
+                "review_verified",
+                "review_verification_level",
+                "review_verification_source",
+                "review_verification_date",
+                "review_source_name",
+                "has_dach_exclusion",
+                "reply_message",
+                "reply_published_date",
+                "reply_updated_date",
             ],
         )
         writer.writeheader()
         writer.writerows(reviews)
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Scrape reviews from Trustpilot for a given domain.")
-    parser.add_argument("domain", help="The domain to scrape reviews for.", type=str)
-    args = parser.parse_args()
+def datetime_converter(o: object) -> str:
+    """
+    Convert a datetime object to a string.
+    :param o: The object to convert.
+    :return: The string representation of the object.
+    """
+    if isinstance(o, datetime):
+        return o.isoformat()
+    raise TypeError(
+        "Object of type '{}' is not JSON serializable".format(o.__class__.__name__)
+    )
 
+
+def write_reviews_to_json(reviews: list[dict], filename: str) -> None:
+    """
+    Write the reviews to a JSON file, converting datetime objects to strings.
+    :param reviews: List of dictionaries containing the review details.
+    :param filename: The name of the JSON file to write the reviews to.
+    """
+    # Convert datetime objects to strings
+    reviews_converted = []
+    for review in reviews:
+        review_converted = {
+            key: datetime_converter(value) if isinstance(value, datetime) else value
+            for key, value in review.items()
+        }
+        reviews_converted.append(review_converted)
+
+    with open(filename, "w") as file:
+        json.dump(reviews_converted, file, indent=4)
+
+
+def sort_reviews(reviews: list[dict], sort_by: str, sort_order: str) -> list[dict]:
+    """
+    Sort the reviews by a specified field in ascending or descending order.
+    :param reviews: List of dictionaries containing the review details.
+    :param sort_by: The field to sort the reviews by.
+    :param sort_order: The order to sort the reviews in ('asc' for ascending, 'desc' for descending).
+    :return: List of dictionaries containing the sorted review details.
+    """
+    reverse = sort_order == "desc"
+    if sort_by and any(review.get(sort_by) for review in reviews):
+        return sorted(reviews, key=lambda x: x.get(sort_by, 0), reverse=reverse)
+    return reviews
+
+
+def main():
+    args = parse_arguments()
     domain = args.domain
     page = 1
     reviews = []
-    all_languages = True
+
+    # Check if allowed by robots.txt
+    ua = UserAgent()
+    user_agent = ua.random
+    if not is_allowed_by_robots_txt(
+        f"https://www.trustpilot.com/review/{domain}", user_agent
+    ):
+        logger.error("Scraping is disallowed by robots.txt.")
+        return
 
     while True:
         try:
-            url = generate_url(domain, page, all_languages)
+            url = generate_url(domain, page, args)
             html = get_html(url)
-            page_reviews = get_reviews(html)
+            if html is None:
+                logger.info(f"Page {page} does not exist. Stopping.")
+                break
+            page_reviews = get_reviews_data(html)
             logger.info(f"Found {len(page_reviews)} reviews on page {page}")
+            if not page_reviews:
+                break
+            page_reviews = process_reviews(page_reviews)
             reviews.extend(page_reviews)
             page += 1
             time.sleep(random.randint(5, 10) / 10)
-        except requests.exceptions.HTTPError:
-            logger.info(f"Reached the last page for {domain}")
+        except requests.exceptions.HTTPError as e:
+            logger.error(f"Error fetching page {page}: {e}")
             break
 
-    write_reviews_to_csv(reviews, f"reviews_{domain}.csv")
-    logger.info(f"Scraped {len(reviews)} reviews for {domain}")
+    if args.sort_by:
+        logger.debug(f"Sorting reviews by {args.sort_by} in {args.sort_order} order.")
+        reviews = sort_reviews(reviews, args.sort_by, args.sort_order)
+
+    if reviews:
+        current_timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        if args.output == "csv" or args.output == "both":
+            csv_filename = f"reviews_{args.domain}_{current_timestamp}.csv"
+            write_reviews_to_csv(reviews, csv_filename)
+            logger.info(f"Reviews saved to CSV file {csv_filename}")
+
+        if args.output == "json" or args.output == "both":
+            json_filename = f"reviews_{args.domain}_{current_timestamp}.json"
+            write_reviews_to_json(reviews, json_filename)
+            logger.info(f"Reviews saved to JSON file {json_filename}")
+    else:
+        logger.info("No reviews scraped.")
 
 
 if __name__ == "__main__":
